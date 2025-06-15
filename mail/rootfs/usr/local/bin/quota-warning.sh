00313ce5e5da6e2b9a14a549be427167ae20f240
Erik Hilton <erikhilton73@gmail.com>
2025-06-15 17:39:08 +0200

    Fix quota warning

--- mail/rootfs/usr/local/bin/quota-warning.sh
+++ mail/rootfs/usr/local/bin/quota-warning.sh
@@ -2,7 +2,7 @@
 # shellcheck disable=SC2086
 PERCENT=$1
 USER=$2
-cat << EOF | /usr/libexec/dovecot/dovecot-lda -d $USER -o "plugin/quota=dict:User quota::noenforcing:proxy::sqlquota"
+cat << EOF | /usr/libexec/dovecot/dovecot-lda -d $USER -o quota_enforce=no
 From: postmaster
 Subject: Quota warning
 
