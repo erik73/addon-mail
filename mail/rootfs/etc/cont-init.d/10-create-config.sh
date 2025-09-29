83d2083717ca799aa8bf6c6c920a6fc431b4d3e7
Erik Hilton <erikhilton73@gmail.com>
2025-09-29 20:03:16 +0200

    Run install.sh

--- mail/rootfs/etc/cont-init.d/10-create-config.sh
+++ mail/rootfs/etc/cont-init.d/10-create-config.sh
@@ -53,6 +53,8 @@ mkdir -p /var/mail/vmail/sieve/global
 chown -R vmailuser:vmail /var/mail
 mkdir -p /var/www/postfixadmin/templates_c
 chown -R nginx: /var/www/postfixadmin
+# Run Postfix install.sh for composer to setup postfixadmin
+/var/www/postfixadmin/install.sh
 
 # Modify config files for S6-logging
 sed -i 's#^ + .*$# + -^auth\\. -^authpriv\\. -mail\\. $T ${dir}/everything#' /etc/s6-overlay/s6-rc.d/syslogd-log/run
