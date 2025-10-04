29f8896f43f061a2e727f96329a6c6cec4fd56eb
Erik Hilton <erikhilton73@gmail.com>
2025-10-04 16:37:40 +0200

    Use virtual dependencies

--- mail/rootfs/etc/cont-init.d/10-create-config.sh
+++ mail/rootfs/etc/cont-init.d/10-create-config.sh
@@ -54,10 +54,6 @@ chown -R vmailuser:vmail /var/mail
 # mkdir -p /var/www/postfixadmin/templates_c
 chown -R nginx: /var/www/postfixadmin
 
-# Run Postfix install.sh for composer to setup postfixadmin
-chmod +x /var/www/postfixadmin/install.sh
-/var/www/postfixadmin/install.sh /var/www/postfixadmin/
-
 # Modify config files for S6-logging
 sed -i 's#^ + .*$# + -^auth\\. -^authpriv\\. -mail\\. $T ${dir}/everything#' /etc/s6-overlay/s6-rc.d/syslogd-log/run
 sed -i 's#^ + .*$# + -^auth\\. -^authpriv\\. -mail\\. $T ${dir}/everything#' /run/service/syslogd-log/run.user
